9cb649dfb3edef154806e72086669dc40f765b28
jwchoi <jwchoi@enliple.com>
2021-12-10 10:05:58 +0900

    no message

--- src/main/java/com/example/springSetting/SpringSettingApplication.java
+++ src/main/java/com/example/springSetting/SpringSettingApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-//test
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.example.*"})
