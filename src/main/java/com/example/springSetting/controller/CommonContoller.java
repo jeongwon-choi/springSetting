8184cff61165eeb0e88ba42d636215c8805e932b
jeongwon-choi <chol0501@nate.com>
2021-12-20 18:28:16 +0900

    데이터 넘기기

--- src/main/java/com/example/springSetting/controller/CommonContoller.java
+++ src/main/java/com/example/springSetting/controller/CommonContoller.java
@@ -2,11 +2,15 @@ package com.example.springSetting.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CommonContoller {
     @RequestMapping(value = "/")
-    public String Home() throws Exception{
-        return "index";
+    public ModelAndView Home() throws Exception{
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("test", "테스트 내용입니다.");
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 }
