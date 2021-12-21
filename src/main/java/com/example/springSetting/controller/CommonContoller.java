431ce8019881e3605df68f9581b2afa5fb8a5a02
jeongwon-choi <chol0501@nate.com>
2021-12-21 12:39:36 +0900

    spring boot + react setting

--- src/main/java/com/example/springSetting/controller/CommonContoller.java
+++ src/main/java/com/example/springSetting/controller/CommonContoller.java
@@ -1,16 +1,12 @@
 package com.example.springSetting.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CommonContoller {
-    @RequestMapping(value = "/")
-    public ModelAndView Home() throws Exception{
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("test", "테스트 내용입니다.");
-        modelAndView.setViewName("index");
-        return modelAndView;
+    @RequestMapping(value = "/home")
+    public String Home() throws Exception{
+        return "Hello World";
     }
 }
